960c0b325396bc34dc1c94938b276c82cf00e36b
jawad-elmous <elmousaouijawad78@gmail.com>
2021-02-04 18:44:08 +0100

    logo , menu , modfi libelle

--- routes/commandes.js
+++ routes/commandes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const Commande = require('../model/commande.model');
 
 router.get('/', async (req, res) => {
-    Commande.getAll((err, results) => {
+    res.render('tables', { title: 'Express' });
+    /*Commande.getAll((err, results) => {
         if (err) throw err;
         else res.render('tables', { results, title: 'Commandes' });
-    });
+    });*/
 });
 module.exports = router;
