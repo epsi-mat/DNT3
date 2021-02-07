3416ee2e61cd9770196bf2a83b7ecf11b9438679
jawadjem <elmousaouijawad78@gmail.com>
2021-02-07 18:49:38 +0100

    ajout des redirections

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const commandeRouter = require('./routes/commandes')
 
 var app = express();
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/commandes', commandeRouter);
 
 // catch 404 and forward to error handler
