fb0ed8646f1753776424026bcda7c0de5d4e9d5c
jawadjem <elmousaouijawad78@gmail.com>
2021-02-07 18:49:38 +0100

    ajout des redirections

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ const Commande = require('../model/commande.model');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PreudHomme' });
 });
 
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Inscription' });
+});
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Connexion' });
+});
 module.exports = router;
+
