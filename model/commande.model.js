a1b6438daee28ddff08736e92f549e7b2830850b
mat-planchot <mat_planchot@hotmail.fr>
2021-02-12 21:29:33 +0100

    add dotenv and ignore .env

--- model/commande.model.js
+++ model/commande.model.js
@@ -1,13 +1,32 @@
 const dbConfig =  require("./db")
 const mysql = require("mysql2");
 
-let pool = mysql.createPool({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
-    port: dbConfig.PORT
-});
+let pool;
+if (process.env.NODE_ENV === 'production') {
+    pool = mysql.createPool({
+        socketPath: dbConfig.HOST,
+        user: dbConfig.USER,
+        password: dbConfig.PASSWORD,
+        database: dbConfig.DB,
+        port: dbConfig.PORT
+    });
+} else if (process.env.NODE_ENV === 'docker') {
+    pool = mysql.createPool({
+        host: dbConfig.DOCKER_HOST,
+        user: dbConfig.USER,
+        password: dbConfig.PASSWORD,
+        database: dbConfig.DB,
+        port: dbConfig.PORT
+    });
+} else {
+    pool = mysql.createPool({
+        host: dbConfig.HOST,
+        user: dbConfig.USER,
+        password: dbConfig.PASSWORD,
+        database: dbConfig.DB,
+        port: dbConfig.PORT
+    });
+}
 
 const Commande = function(commande) {
     this.nom_produit = commande.nom_produit
--- model/db.js
+++ model/db.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-    HOST: process.env.MYSQL_HOSTNAME,
-    PORT: process.env.PORT,
+    HOST: process.env.MYSQL_HOST,
+    DOCKER_HOST: process.env.MYSQL_HOSTNAME,
+    PORT: process.env.MYSQL_PORT,
     USER: process.env.MYSQL_USER,
     PASSWORD: process.env.MYSQL_PASSWORD,
-    DB: process.env.MYSQL_DATABASE
+    DB: process.env.MYSQL_DATABASE,
+    SOCKET: process.env.MYSQL_SOCKET
 };
