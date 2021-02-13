0b6da936492a1f7d2229bde3b617d6253a210e69
Yoann <yoann.clement@epsi.fr>
2021-02-13 21:21:45 +0100

    MAJ BDD
    Ajout produit et custom commande

---
+++ sqlscripts/epsidb.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 13, 2021 at 08:19 PM
+-- Server version: 8.0.21
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `epsi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id_commande` int NOT NULL AUTO_INCREMENT,
+  `date_commande` datetime NOT NULL,
+  `date_livraison` datetime DEFAULT NULL,
+  `quantite` int NOT NULL,
+  `id_produit` int NOT NULL,
+  PRIMARY KEY (`id_commande`),
+  KEY `id_produit` (`id_produit`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `commande`
+--
+
+INSERT INTO `commande` (`id_commande`, `date_commande`, `date_livraison`, `quantite`, `id_produit`) VALUES
+(1, '2021-02-02 15:08:51', NULL, 20, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id_produit` int NOT NULL AUTO_INCREMENT,
+  `produit` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `prix_hors_taxe` float(7,2) NOT NULL,
+  `tva` float(7,2) NOT NULL,
+  `fournisseur` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_produit`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `produit`
+--
+
+INSERT INTO `produit` (`id_produit`, `produit`, `prix_hors_taxe`, `tva`, `fournisseur`) VALUES
+(1, 'papier', 20.00, 20.00, 'Paper Industry'),
+(2, 'lecteur de carte', 150.00, 20.00, 'MasterCard'),
+(3, 'Traiteur', 1200.00, 10.00, 'JP');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
