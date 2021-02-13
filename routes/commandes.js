1f84b83ddf7f028219955f703db6f6c82266ddb9
jawadjem <elmousaouijawad78@gmail.com>
2021-02-13 17:43:42 +0100

    Changement front
    creation fichier header
    ajout des boutons editer supprimer et ajouter produit
    amelioration de la condition pour format date (ternaire au lieu de if)

--- routes/commandes.js
+++ routes/commandes.js
@@ -6,7 +6,14 @@ const Commande = require('../model/commande.model');
 router.get('/', async (req, res) => {
     Commande.getAll((err, results) => {
         if (err) throw err;
-        else res.render('tables', { results: results, title: 'Commandes' });
+        else res.render('tables', {results: results, title: 'Commandes'});
+    });
+});
+
+router.get('/edit', async (req, res) => {
+    Commande.getAll((err, results) => {
+        if (err) throw err;
+        else res.render('edit_commandes', {results: results, title: 'Editer Produit'});
     });
 });
 module.exports = router;
